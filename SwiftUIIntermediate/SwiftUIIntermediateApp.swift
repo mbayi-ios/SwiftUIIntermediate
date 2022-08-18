2ac4e2dacda1b417ccab56c8f6c366e9655e2b88
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-18 22:27:06 +0300

    Initial Commit

---
+++ SwiftUIIntermediate/SwiftUIIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntermediateApp.swift
+//  SwiftUIIntermediate
+//
+//  Created by Amby on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
